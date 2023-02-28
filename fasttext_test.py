ed11bcc52de37a405846cdad3767c3aefe2d2d14
kaiyamag <kmagnuson24@wooster.edu>
2023-02-28 11:42:41 -0500

    Add get_vectorized_str function to convert a list of functions to a list of vectors

--- fasttext_test.py
+++ fasttext_test.py
@@ -4,14 +4,12 @@ import io
 # Limited to 300,000 for debugging
 NUM_WORDS = 300000
 
-# TODO:
-# Make function to get vector for a given word, else return ???
-# Make function to make list of vectors from list of words/tokens
+# Maximum length of text to process
+MAX_STR_LENGTH = 30
 
 class InputProcessor:
     # Attributes:
     ft_filename = ""   # FastText word embeddings file
-    # tokenized_str    # A list of Tokens
     vectorized_str = []  # A list of word vectors corresponding to words from tokenized_str
     vector_data = {}    # A dictionary where the key is a string and the value is a list of floats (300-D vector)
 
@@ -54,11 +52,36 @@ class InputProcessor:
             print(word, "is in the dictionary")
             return list(self.vector_data[word])     # Must convert map object to list before accessing
         else:
+            # TODO: Should this return an empty list or a random vector? (check literature)
             print(word, "is not in the dictionary")
             empty = []
             for i in range(300):
                 empty.append(0.0)
             return empty
+
+
+    # Takes a list of up to MAX_STR_LENGTH tokens (as strings). Populates and returns vectorized_str,
+    # a list of vector representations of those tokens, filled with empty vectors if less 
+    # than MAX_STR_LENGTH tokens were grabbed.
+    def get_vectorized_str(self, tokenized_str):
+        strlen = 0
+        for token in tokenized_str:
+            if (strlen < MAX_STR_LENGTH):
+                self.vectorized_str.append(self.get_vector(token))
+            else:
+                break
+            strlen = strlen + 1
+
+        # Make empty 300-D vector    
+        empty = []
+        for i in range(300):
+            empty.append(0.0)
+        
+        # Fill remaining list spots with empty vectors, up to capacity of MAX_STR_LENGTH
+        for i in range(MAX_STR_LENGTH - strlen):
+            self.vectorized_str.append(empty)
+        
+        return self.vectorized_str
     
 
     # Getter method. Returns dictionary of vector data
@@ -78,10 +101,16 @@ def main():
     print("--------------------------------------")
 
     # Test word vector retrieval 
-    sample_vec = processor.get_vector("quite")
-    print(sample_vec)
-    test_word = "fdsa"
-    print(test_word, len(processor.get_vector(test_word)))
+    # sample_vec = processor.get_vector("quite")
+    # print(sample_vec)
+    # test_word = "fdsa"
+    # print(test_word, len(processor.get_vector(test_word)))
+
+    # Test conversion from list of words to list of vectors
+    sample_str = ["quite", "woman", "James"] 
+    vectorized_str = processor.get_vectorized_str(sample_str)
+    print("Vectorized string:", vectorized_str)
+    print("length:", len(vectorized_str))
 
 
 if __name__ == '__main__':
