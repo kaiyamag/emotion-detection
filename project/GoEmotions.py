8a074379b32eedd3041c371b766d5a287671bf19
kaiyamag <kmagnuson24@wooster.edu>
2023-03-31 17:39:14 -0400

    Add function to build x_train dataset of vectorized comments

--- project/GoEmotions.py
+++ project/GoEmotions.py
@@ -3,6 +3,8 @@ import tensorflow_datasets as tf_datasets
 
 from itertools import islice
 
+from InputProcessor import InputProcessor
+
 
 """ This class stores the loaded GoEmotions database and manages functions to extract data from this dataset
 """
@@ -10,10 +12,17 @@ class GoEmotions:
     # Attributes
     dataset = []
     master_emotions = ['admiration',  'amusement',  'anger', 'annoyance', 'approval', 'caring', 'confusion', 'curiosity',  'desire', 'disappointment', 'disapproval',  'disgust', 'embarrassment',  'excitement',  'fear', 'gratitude',  'grief', 'joy',  'love', 'nervousness', 'neutral', 'optimism', 'pride', 'realization', 'relief', 'remorse',  'sadness',  'surprise']
+    
+    # For buildXTrain()
+    x_train = []
+    filename = "C:\\Users\\aeble\\Documents\\CS_200_Projects\\Junior_IS\\wiki-news-300d-1M.vec"
+    input_processor = []
+
 
     # Initializer
     def __init__(self):
         self.dataset = tf_datasets.load('goemotions', split='train')
+        self.input_processor = InputProcessor(self.filename)
     
 
     """ Takes an index of this dataset. Returns the comment text of the datapoint at the given index. 
@@ -49,6 +58,7 @@ class GoEmotions:
 
         return emotions
     
+    
     """ Returns a list of all emotions present (value of 1) in the given emotion one-hot-encoding vector
     """
     def get_one_hot_emotions(self, vec):
@@ -65,3 +75,30 @@ class GoEmotions:
                 emotions.append(self.master_emotions[i])
         
         return emotions
+
+
+    """ Converts each extracted comment from the entire GoEmotions dataset to a list of vectorized comments
+    """
+    def buildXTrain(self):
+        # For each entry in GoEmotions database, extract comment and convert to vector
+        iterator = iter(self.dataset)
+        for i in range(0, len(self.dataset)):
+            element = next(iterator)
+            comment_text = element['comment_text'].numpy()
+            comment_text = comment_text.decode("utf-8")
+            comment_text = self.input_processor.tokenize(comment_text)
+            
+            comment_vec = self.input_processor.get_vectorized_str(comment_text)
+
+            self.x_train.append(comment_vec)
+
+            # Print debugging message:
+            if ((i % 1000) == 0):
+                print("... ", i, " / ", len(self.dataset))
+        
+        print("First 5 elements of x_train:")
+        print(self.x_train[1])
+
+        # TODO: Program pauses while trying to print here... why?
+
+
