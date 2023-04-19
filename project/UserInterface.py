d020e934d0f22e6c797369dcb19c59fe34c8b8cb
kaiyamag <kmagnuson24@wooster.edu>
2023-04-18 21:48:09 -0400

    Fix OOP error in setup_model. Add documentation comments

--- project/UserInterface.py
+++ project/UserInterface.py
@@ -1,3 +1,6 @@
+# UserInterface.py
+# Kaiya Magnuson, April 2023
+
 import io
 import numpy as np
 import math
@@ -7,32 +10,32 @@ from model import Model
 """ Manages the user interface for interacting with the LSTM model. 
 """
 class UserInterface:
-    # Attributes:
-    
     # Initializer
     def __init__(self):
         self.model = Model()
     
+
+    """ Prepares the model for predictions by loading training datasets, building, and training the model.
+    """
     def setup_model(self):
         self.model.build_train_sets()
 
         test_split = 0.2
-        self.build_test_sets(test_split)
+        self.model.build_test_sets(test_split)
         self.model.build_model()
         self.model.train_model(self.model)
     
 
+""" Prompts the user for text to enter into the emotion detection model. Prints a list of emotions 
+predicted for that text sample.
+"""
 def main():
     print("Welcome to the LSTM model for detecting emotion in text!")
     print("Loading model...")
 
     ui = UserInterface()
     my_model = ui.model
-    my_model.build_train_sets()
-    test_split = Model.test_split
-    my_model.build_test_sets(test_split)
-    my_model.build_model()
-    my_model.train_model(my_model)
+    ui.setup_model()
 
     prompt = "Please enter a sentence between 1 and 30 words or press 'q' to quit:\n"
     take_input = True
@@ -51,6 +54,7 @@ def main():
         pred = Model.to_binary(my_model.get_pred(vec)[0])
         emotions = my_model.ge.get_one_hot_emotions(pred)
         print("Predicted emotions:", emotions)
+
 
 if __name__ == '__main__':
     main()
