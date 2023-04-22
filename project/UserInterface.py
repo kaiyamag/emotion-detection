98ec59f3ff0c4ef31afca54ba31ed03ceddd9d6b
kaiyamag <kmagnuson24@wooster.edu>
2023-04-22 16:41:16 -0400

    Add functionality to switch between training new model and loading saved model

--- project/UserInterface.py
+++ project/UserInterface.py
@@ -11,11 +11,17 @@ from model import Model
 """
 class UserInterface:
     # Initializer
-    def __init__(self, filepath, model_file):
-        self.model = Model(filepath, model_file)
+    def __init__(self, fastText, model_file, train_new):
+        self.model_file = model_file
+        
+        if (train_new):
+            self.model = Model(fastText, False)
+            self.setup_model()
+        else:
+            self.model = Model(fastText, model_file)
     
 
-    """ Prepares the model for predictions by loading training datasets, building, and training the model.
+    """ Prepares the model for predictions by loading training datasets, building, training, and saving the model.
     """
     def setup_model(self):
         self.model.build_train_sets()
@@ -24,6 +30,7 @@ class UserInterface:
         self.model.build_test_sets(test_split)
         self.model.build_model()
         self.model.train_model()
+        self.model.save_model(self.model_file)
     
 
 """ Prompts the user for text to enter into the emotion detection model. Prints a list of emotions 
@@ -33,15 +40,21 @@ def main():
     print("Welcome to the LSTM model for detecting emotion in text!")
     print("Loading model...")
 
+    #-------------- CHANGE WHEN RUNNING ON NEW COMPUTER -------------- 
+
+    # fastText_file: the filepath of the FastText word embeddings dataset. Required if training a new model
     fastText_file = "C:\\Users\\aeble\\Documents\\CS_200_Projects\\Junior_IS\\wiki-news-300d-1M.vec"
+
+    # model_file: the filepath of a saved Keras model, or the desired file location for a new model to be saved
     model_file = "C:\\Users\\aeble\\Documents\\CS_200_Projects\\Junior_IS\\emotion-detection\\model"
 
-    ui = UserInterface(fastText_file, model_file)
-    my_model = ui.model
+    # train_new: Set to true to train a new model and save at `model_file`, set to false to use model saved at `model_file`
+    train_new = False
 
-    # Train model if no saved model was loaded
-    if (not model_file):
-        ui.setup_model()
+    # ----------------------------------------------------------------
+
+    ui = UserInterface(fastText_file, model_file, train_new)
+    my_model = ui.model
 
     prompt = "Please enter a sentence between 1 and 30 words or press 'q' to quit:\n"
     take_input = True
